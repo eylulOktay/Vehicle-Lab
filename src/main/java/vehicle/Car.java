4b51425fd72bae9e358ff584a64878948a483f2e
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2023-03-10 10:26:03 -0500

    created function framework stuff in Car

--- src/main/java/vehicle/Car.java
+++ src/main/java/vehicle/Car.java
@@ -1,5 +1,41 @@
 package vehicle;
+import java.util.List;
 
 public class Car {
-    
+    public Car(String make, String model, double startingMileage){
+
+    }
+
+    public Car(String make, String model){
+
+    }
+
+    public boolean canDrive(double miles){
+        return false;
+
+    }
+    public void drive(double miles){
+
+    }
+    public String toString(){
+        return "";
+    }
+    public double getMileage(){
+        return 0;
+    }
+    public String getMake(){
+        return "";
+    }
+    public String getModel(){
+        return "";
+    }
+    public double getRemainingRange(){
+        return 0;
+    }
+    protected void addMileage(double miles){
+
+    }
+    public int roadTrip(List<Double> milesEachDay){
+        return 0;
+    }
 }
