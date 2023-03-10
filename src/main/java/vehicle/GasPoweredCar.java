7b44bf5365c18551ab94f7df896f72f9d408318c
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2023-03-10 10:30:41 -0500

    got rid of errors

--- src/main/java/vehicle/GasPoweredCar.java
+++ src/main/java/vehicle/GasPoweredCar.java
@@ -2,11 +2,12 @@ package vehicle;
 
 public abstract class GasPoweredCar extends Car {
     public GasPoweredCar(String make, String model, double startingMileage, double mpg, double fuelCapacityGallons) {
+        super(make,model);
         System.out.println();
     }
 
     public GasPoweredCar (String make, String model, double mpg, double fuelCapacityGallons){
-
+        super(make,model);
     }
 
     public void drive(double miles) {
@@ -14,15 +15,15 @@ public abstract class GasPoweredCar extends Car {
     }
 
     public double getMPG() {
-        System.out.println();
+        return 0;
     }
 
     public double getFuelLevel() {
-        System.out.println();
+        return 0;
     }
 
     public double getFuelCapacity() {
-        
+        return 0;
     } 
 
     public void refillTank()  {
@@ -30,7 +31,7 @@ public abstract class GasPoweredCar extends Car {
     }
 
     public double getRemainingRange() {
-
+        return 0;
     }
 
     public void refillTank(double gallons) {
