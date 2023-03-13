f18c1ae9d7da8ca3ed7e7e6d9b5542b40b3fda07
Eylul Oktay <gulzadeoktay@gmail.com>
2023-03-13 10:46:48 -0400

    chevvy done

---
+++ src/main/java/vehicle/ChevroletBird.java
@@ -0,0 +1,47 @@
+package vehicle;
+
+public class ChevroletBird extends ElectricCar implements Flying{
+
+    private boolean WingsExtended = false;
+
+    public ChevroletBird(double startingMileage){
+        super("Cheverolet", "Bird",startingMileage, 250);
+        WingsExtended = false;
+    }
+
+    public ChevroletBird(){
+        this(0);
+    }
+
+    public boolean checkWingsExtended(){
+        if (WingsExtended == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void drive(double miles){
+        WingsExtended = false;
+        super.drive(miles);
+    }
+
+    public boolean canFly(double miles){
+        if(miles < 0){
+            throw new IllegalArgumentException();
+        }
+
+        return true;
+    }
+
+    public void fly(double miles){
+        if(miles < 0){
+            throw new IllegalArgumentException();
+        }
+
+        if(miles > getRemainingRange()){
+            throw new IllegalArgumentException();
+        }
+    }
+}
