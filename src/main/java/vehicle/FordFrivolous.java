3c8690cfe99fcca31d035b346a6b035ae3e20c72
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2023-03-13 10:44:33 -0400

    some ford

--- src/main/java/vehicle/FordFrivolous.java
+++ src/main/java/vehicle/FordFrivolous.java
@@ -1,10 +1,28 @@
 package vehicle;
 
-public class FordFrivolous extends GasPoweredCar{
+public class FordFrivolous extends GasPoweredCar implements Flying, SelfDriving{
     public FordFrivolous(double startingMileage){
         super("Ford", "Frivolous",startingMileage,23.6,20);
     }
     public FordFrivolous(){
         this(0);
+    }
+    public void driveAutonomously(double miles){
+        if (canDrive(miles*2)){
+            decreaseFuelLevel(getMPG()/miles);
+            drive(miles);
+        }
+        else{
+            drive(getRemainingRange()/2);
+            decreaseFuelLevel(getMPG()/getRemainingRange());
+        }
+
+    }
+    public boolean canFly(double miles){
+        return canDrive(miles*3);
+    }
+
+    public void fly(double miles){
+        
     }
 }
